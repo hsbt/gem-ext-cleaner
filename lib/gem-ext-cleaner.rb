edfd01aa28257a1646f73d1fb03207695d9aff44
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-05-28 17:42:20 +0900

    We need to cleanup manually

---
+++ lib/gem-ext-cleaner.rb
@@ -0,0 +1,16 @@
+require "rbconfig"
+require "pathname"
+
+module GemExtCleaner
+  def self.clean(spec)
+    (spec.full_require_paths - [spec.extension_dir]).each do |path|
+      Dir.glob(File.join(path, "**/*")).each do |file|
+        if Pathname(file).extname == ".#{RbConfig::CONFIG["DLEXT"]}"
+          File.delete(file)
+          puts "Delete #{file}"
+        end
+      end
+    rescue Errno::EPERM
+    end
+  end
+end
--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,17 +1,5 @@
-# このプラグインは gem のインストール後に実行される
-# このプラグインは `lib` の下にコピーされた foo.so や foo.bundle などのファイルを削除する
-
-require "rbconfig"
-require "pathname"
+require "gem-ext-cleaner"
 
 Gem.post_install do |installer|
-  (installer.spec.full_require_paths - [installer.spec.extension_dir]).each do |path|
-    Dir.glob(File.join(path, "**/*")).each do |file|
-      if Pathname(file).extname == ".#{RbConfig::CONFIG["DLEXT"]}"
-        File.delete(file)
-        puts "Delete #{file}"
-      end
-    end
-  end
-rescue Errno::EPERM
+  GemExtCleaner.clean(installer.spec)
 end
