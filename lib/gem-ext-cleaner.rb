7407aa26bdae62422a8fb788be406afaee6dca26
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-05-28 18:47:13 +0900

    Skip to delete extensions with fat gem

--- lib/gem-ext-cleaner.rb
+++ lib/gem-ext-cleaner.rb
@@ -5,6 +5,9 @@ module GemExtCleaner
   def self.clean(spec)
     (spec.full_require_paths - [spec.extension_dir]).each do |path|
       Dir.glob(File.join(path, "**/*")).each do |file|
+        # Prevent deleting files in versioned directories like google-protobuf, nokogiri
+        next if file =~ /(\d+\.\d+\/)/
+
         if Pathname(file).extname == ".#{RbConfig::CONFIG["DLEXT"]}"
           File.delete(file)
           puts "Delete #{file}"
