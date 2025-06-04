64f63443f2e034ce11cd430674078f9270d06d5e
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-06-04 18:10:38 +0900

    Support old version of Ruby

--- lib/gem-ext-cleaner.rb
+++ lib/gem-ext-cleaner.rb
@@ -4,16 +4,18 @@ require "pathname"
 module GemExtCleaner
   def self.clean(spec)
     (spec.full_require_paths - [spec.extension_dir]).each do |path|
-      Dir.glob(File.join(path, "**/*")).each do |file|
-        # Prevent deleting files in versioned directories like google-protobuf, nokogiri
-        next if file.sub(/#{spec.full_gem_path}\//, "") =~ /(\d+\.\d+\/)/
+      begin
+        Dir.glob(File.join(path, "**/*")).each do |file|
+          # Prevent deleting files in versioned directories like google-protobuf, nokogiri
+          next if file.sub(/#{spec.full_gem_path}\//, "") =~ /(\d+\.\d+\/)/
 
-        if Pathname(file).extname == ".#{RbConfig::CONFIG["DLEXT"]}"
-          File.delete(file)
-          puts "Delete #{file}"
+          if Pathname(file).extname == ".#{RbConfig::CONFIG["DLEXT"]}"
+            File.delete(file)
+            puts "Delete #{file}"
+          end
         end
+      rescue Errno::EPERM
       end
-    rescue Errno::EPERM
     end
   end
 end
